a70e1e2dabad2c0f830f64e9c3963574837dcc87
yuko fuyutsuki <yukofuyutsuki@gmail.com>
2018-10-08 21:54:29 +0900

    Ah..... my fault....

--- src/tokyo/pmmp/Texter/Core.php
+++ src/tokyo/pmmp/Texter/Core.php
@@ -174,11 +174,9 @@ class Core extends PluginBase implements Listener {
   private function checkPackaged(): bool {
     $cl = Lang::fromConsole();
     if ($this->isPhar()) {
-      if (class_exists("\\tokyo\\pmmp\\Texter\\libs\\tokyo\\pmmp\\libform")) {
-        var_dump("packaged");
+      if (class_exists("\\tokyo\\pmmp\\Texter\\libs\\tokyo\\pmmp\\libform\\FormApi")) {
         return true;// PoggitCI
       }else {
-        var_dump("not_packaged");
         $message = $cl->translateString("error.on.enable.not.packaged");
         $this->getLogger()->critical($message);
         $this->getServer()->getPluginManager()->disablePlugin($this);
